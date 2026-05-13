4a12ea379aae6af62e7f9c9d3615c1b313fe398c
TumiTarantino <tuminthau@gmail.com>
2026-05-13 16:08:58 +0200

    Added database.py with SQLAlchemy initial stuff

---
+++ app/database.py
@@ -0,0 +1,18 @@
+#These are the recommended imports that the current FastAPI Documentation reccommends,but...
+#from typing import Annotated
+#from fastapi import Depends, FastAPI, HTTPException, Query
+#from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+#This is what we'll use as we're following the tutorial
+from sqlalchemy import create_engine 
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.orm import sessionmaker
+
+#This declaration is bad practice somehow(Maybe password?)
+SQLALCHEMY_DATABASE_URL = 'postgresql:postgres:tumitino@localhost/FastAPI'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
