3c8e237b97c22c0b0fd143c971305f6104092888
TumiTarantino <tuminthau@gmail.com>
2026-05-14 15:13:45 +0200

    Added SQLAlchemy ORM to routes and have been fully updated

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from multiprocessing import synchronize
 from typing import Optional
 
 from fastapi import FastAPI, HTTPException, Response, status, Depends
@@ -14,7 +15,7 @@ from . import models
 from sqlalchemy.orm import Session
 from .database import engine, get_db
 
-models.Base.metadata.create_all(bind=engine)  
+models.Base.metadata.create_all(bind=engine)  # type: ignore
 
 #Global variables
 app = FastAPI()
@@ -43,6 +44,7 @@ while True:#Tries untils connection is successful then breaks out of loop
 #defines how a post should look
 # Makes sure the value is a str and makes sure that title and content are explicitly used
 #FastAPI is noice
+#This class extends Pydantic's BaseModel and will thus be a Pydantic Model
 class Post(BaseModel):
     title: str
     content: str
@@ -56,57 +58,81 @@ def root():
 
 @app.get("/sqll")
 def test_sql(db: Session= Depends(get_db)):
-    return {"Status":"success"}
+    temp = db.query(models.Post).all()
+    return {"Status":temp}
 
 
 
 @app.get("/posts/{id}")
-def get_post(id: int,):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s """,(id,))
-    post = cursor.fetchone()
+def get_post(id: int, db: Session= Depends(get_db)):
+    #Commented because of ORM, don't delete
+    #cursor.execute("""SELECT * FROM posts WHERE id = %s """,(id,))
+    #post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first() #type: ignore
+    
     if not post:
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"post with {id} is not found")
     return {"data" : post}
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts """)
-    posts = cursor.fetchall()
-    return {"data" : posts}
+def get_posts(db: Session= Depends(get_db)):
+    #Commented out since we are using ORMs now, don't delete
+    #cursor.execute("""SELECT * FROM posts """)
+    #posts = cursor.fetchall()
+
+    #ORMS way
+    posts = db.query(models.Post).all()
+    return {"data":posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post:Post):
-    #Try to sanitize the statement(NO SQL INJECTION PLZ)
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()#Saves the executed result in a variable, fetchall gets the result of the execute
-    conn.commit()# Saves changes to the actual database
+def create_post(post:Post,db: Session= Depends(get_db)):
+    #Try to sanitize the statement(NO SQL INJECTION PLZ)[Commented out because ORM, don't delete]
+    #cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+    #                (post.title, post.content, post.published))
+    #new_post = cursor.fetchone()#Saves the executed result in a variable, fetchall gets the result of the execute
+    #conn.commit()# Saves changes to the actual database
+
+    #ORM way:
+    new_post = models.Post(**post.dict())#use post.model_dump after tutorial
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"data": new_post}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """,(id,))
-    deleted_post = cursor.fetchone()
-    
+def delete_post(id: int,db: Session= Depends(get_db)):
+    #Replaced with ORM logic, don't delete
+    #cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING * """,(id,))
+    #deleted_post = cursor.fetchone()
+    #conn.commit() (put after the exception check but it commented out now)
+    post = db.query(models.Post).filter(models.Post.id == id).first() #type: ignore
 
-    if deleted_post is None:
+    if post is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} does not exist"
         )
-    conn.commit()
+    #post.delete(synchronize_session=False)
+    db.delete(post)
+    db.commit()
     #Apparently if you delete data or 204 is the status code, you don't want to return anything
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title= %s, content= %s, published= %s WHERE id = %s RETURNING * """,
-                   (post.title, post.content, post.published, id,))
-    updated_post = cursor.fetchone()
-    if updated_post is None:
+def update_post(id: int, updated_post: Post,db: Session= Depends(get_db)):
+    #Replaced with ORM, don't delete
+    #cursor.execute("""UPDATE posts SET title= %s, content= %s, published= %s WHERE id = %s RETURNING * """,
+    #              (post.title, post.content, post.published, id,))
+    #updated_post = cursor.fetchone()
+    #conn.commit()
+    post_query = db.query(models.Post).filter(models.Post.id == id)#type: ignore
+    post = post_query.first()
+    if post is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} does not exist"
         )
-    conn.commit()
-    return{"data": updated_post}
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    
+    return{"data": post_query.first()}
--- app/models.py
+++ app/models.py
@@ -1,10 +1,18 @@
+from psycopg import Timestamp
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
+#This class is not the Pydantic model
+#It is the sqlalchemy model and defines how a table looks like
+#This one creates the columns and is for querying, creating, updating and deleting entries within the database
 class Post(Base): #Extends the Base declared in the database file.
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()')) 
+    
