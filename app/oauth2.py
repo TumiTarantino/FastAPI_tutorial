c19f92013b42cbfb6baafec15e5fd3dc58725c68
TumiTarantino <tuminthau@gmail.com>
2026-05-21 12:01:56 +0200

    Fixed Http Exceptions, added get current user to oauth2

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,6 +1,11 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+#The Login is from your login route in auth.py
+oath2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 #Check FASTAPI documentation if you want more details
 
 #We need 3 things: SECRET_KEY, Algorithm, Expiration Time
@@ -12,7 +17,8 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    #utcnow is deprecated...
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode,SECRET_KEY ,algorithm=ALGORITHM)
     
@@ -20,12 +26,19 @@ def create_access_token(data: dict):
 
 def verify_access_token(token: str, credentials_exception):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
-        id: str = payload.get("users_id") #type: ignore
+        id: str = payload.get("user_id") #type: ignore
 
         if id is None:
             raise credentials_exception
         token_data = schemas.TokenData(id=id) 
     except JWTError:
         raise credentials_exception
+    return token_data
+    
+def get_current_user(token: str = Depends(oath2_scheme)):
+    #don't ask me about this function, i am confused too
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials",
+                                          headers={"WWW-Authenticate":"Bearer"})
+    return verify_access_token(token, credentials_exception)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -19,10 +19,10 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first() #type: ignore
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invaild Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invaild Credentials")
     
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invaild Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invaild Credentials")
     #TODO: Create JWT Token, return token
     access_token = oauth2.create_access_token(data= {"user_id":user.id})
     return {"access_token": access_token, "token_type": "bearer"}
