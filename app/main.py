fbc32f352cde8265f330f8751d485ed87735c942
TumiTarantino <tuminthau@gmail.com>
2026-05-26 18:40:17 +0200

    Cleaned up main

--- app/main.py
+++ app/main.py
@@ -1,16 +1,8 @@
-from fastapi import FastAPI, HTTPException, Response, status, Depends
-from typing import List
-#from fastapi.params import Body : works fine, but My Vscode is not happy
-from pydantic import BaseModel
+from fastapi import FastAPI
 #the randrange is cause we have no database and have to assign an id
-from random import randrange
-import time
-import psycopg2 # Database Driver
-from psycopg2.extras import RealDictCursor
-from . import models, schemas, utils
-from sqlalchemy.orm import Session
-from .database import engine, get_db
-#And this is when it all changed...
+#from random import randrange --goodtimes
+from . import models
+from .database import engine
 from .routers import post, user, auth
 
 
@@ -19,29 +11,6 @@ models.Base.metadata.create_all(bind=engine)  # type: ignore
 
 #Global variables
 app = FastAPI()
-
-
-
-
-
-        
-#----------------DATABASE STUFF---------------------------------------------------
-while True:#Tries untils connection is successful then breaks out of loop
-    try:
-        # stuff is hardcoded which is eww, will change later since we dk how yet, ooof,
-        conn = psycopg2.connect(host = 'localhost', database='FastAPI', user='postgres', password='tumitino',
-                               cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successful!")
-        break
-    except Exception as error:
-        print("Connection to database was unsuccessful.")
-        print("Error", error) #Prints what was caught
-        time.sleep(3)# waits for 3 seconds until restarting loop
-    
-#----------------DATABASE END-----------------------------------------------------
-
-
 
 #App routers
 app.include_router(post.router)
