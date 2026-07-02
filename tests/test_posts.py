f81b05de193c4fe66fe0ec023f2a823a796df197
TumiTarantino <tuminthau@gmail.com>
2026-07-02 09:25:36 +0200

    Added 3 tests in test posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,5 +1,25 @@
+from app import schemas
+
+#test_posts fixture uses test_user as an arg, so all test_posts already have a user
 def test_get_all_posts(authorized_client, test_posts):
     response = authorized_client.get("/posts/")
-    print(response.json())
+    def validate(post):
+        return schemas.PostOut(**post)
+    
+    #hm idk what this does?
+    posts_map = map(validate, response.json())
+
     assert len(response.json()) == len(test_posts)
     assert response.status_code == 200
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    response = client.get("/posts/")
+    assert response.status_code == 401
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    response = client.get(f"/posts/{test_posts[0].id}")
+    assert response.status_code == 401
+
+def test_get_one_non_existing_post(authorized_client, test_posts):
+    response = authorized_client.get("/posts/67677997")
+    assert response.status_code == 404
