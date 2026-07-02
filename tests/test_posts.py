ef2c0c87326d02899667362a830c9f3aee605344
TumiTarantino <tuminthau@gmail.com>
2026-07-02 09:54:53 +0200

    Added 1 test in test posts and added response model in get one post

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -23,3 +23,11 @@ def test_unauthorized_user_get_one_post(client, test_posts):
 def test_get_one_non_existing_post(authorized_client, test_posts):
     response = authorized_client.get("/posts/67677997")
     assert response.status_code == 404
+
+def test_get_one_post(authorized_client, test_posts):
+    response = authorized_client.get(f"/posts/{test_posts[0].id}")
+    print(response.json())
+    post = schemas.PostOut(**response.json())
+    assert response.status_code == 200
+    assert post.post.id == test_posts[0].id
+    assert post.post.content == test_posts[0].content
